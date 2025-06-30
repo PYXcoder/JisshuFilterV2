58f6d4a735c1444631e7615ff41582fcc9039e87
PYXcoder <yadu.ji8318@gmail.com>
2025-06-30 14:22:02 +0530

    Sync changes 🥀

--- Template.py
+++ Template.py
@@ -1,4 +1,4 @@
-import os
+
 
 class jisshu_template(object):
     JISSHU_NAME = "Jisshu"
@@ -8,18 +8,16 @@ class jisshu_template(object):
         "You can report files or videos that contain issues like copyright infringement, +18 content, violence, etc."
     )
 
-    JISSHU_REPORT_LINK = "https://t.me/Jisshu_support"
+    JISSHU_REPORT_LINK = "https://t.me/YaduvanshiXsupport"
 
     JISSHU_COLOURS = {
-    "header": {"background": "#48cffe", "color": "#ffffff"},
-        
-    "mx_player": {"background": "#8399ff", "color": "#ffffff"},
-    "vlc_player": {"background": "#00e929", "color": "#000000"},
-    "playit_player": {"background": "#ff9900", "color": "#000000"},
-    "download": {"background": "#00c853", "color": "#ffffff"},
-    "create_link": {"background": "#ffd700", "color": "#000000"},
-    "share": {"background": "#ff1744", "color": "#ffffff"},
-        
-    "file_information": {"background": "#292b2c", "color": "#ffffff"},
-    "description": {"background": "#fff2c8", "color": "#6c4c00"}
+        "header": {"background": "#48cffe", "color": "#ffffff"},
+        "mx_player": {"background": "#8399ff", "color": "#ffffff"},
+        "vlc_player": {"background": "#00e929", "color": "#000000"},
+        "playit_player": {"background": "#ff9900", "color": "#000000"},
+        "download": {"background": "#00c853", "color": "#ffffff"},
+        "create_link": {"background": "#ffd700", "color": "#000000"},
+        "share": {"background": "#ff1744", "color": "#ffffff"},
+        "file_information": {"background": "#292b2c", "color": "#ffffff"},
+        "description": {"background": "#fff2c8", "color": "#6c4c00"},
     }
--- info.py
+++ info.py
@@ -1,8 +1,9 @@
 import re
-from os import environ,getenv
+from os import environ
 from Script import script
 
-id_pattern = re.compile(r'^.\d+$')
+id_pattern = re.compile(r"^.\d+$")
+
 
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
@@ -11,102 +12,164 @@ def is_enabled(value, default):
         return False
     else:
         return default
-    
+
+
 # Main
-SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '29728878'))
-API_HASH = environ.get('API_HASH', 'a961168f7807061e77e1fb39c3f6ef71')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
-PORT = environ.get('PORT', '8082')
+SESSION = environ.get("SESSION", "Media_search")
+API_ID = int(environ.get("API_ID", "29728878"))
+API_HASH = environ.get("API_HASH", "a961168f7807061e77e1fb39c3f6ef71")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+PORT = environ.get("PORT", "8082")
 
-# Owners 
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5294914915 7986684117').split()]
-OWNER_USERNAME = environ.get('OWNER_USERNAME', 'B4UOwnerBot') # without @ or https://t.me/ 
-USERNAME = environ.get('USERNAME', "MovieXprovider") # ADMIN USERNAME
+# Owners
+ADMINS = [
+    int(admin) if id_pattern.search(admin) else admin
+    for admin in environ.get("ADMINS", "5294914915 7986684117").split()
+]
+OWNER_USERNAME = environ.get(
+    "OWNER_USERNAME", "B4UOwnerBot"
+)  # without @ or https://t.me/
+USERNAME = environ.get("USERNAME", "MovieXprovider")  # ADMIN USERNAME
 
-# Database Channel 
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002150820537 -1002235533210').split()]
+# Database Channel
+CHANNELS = [
+    int(ch) if id_pattern.search(ch) else ch
+    for ch in environ.get("CHANNELS", "-1002150820537 -1002235533210").split()
+]
 
 # ForceSub Channel & Log Channels
-AUTH_CHANNEL = int(environ.get('AUTH_CHANNEL', ''))
-AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', ''))
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002179479692'))
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1002470952565')) 
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1002471108414'))
+AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", ""))
+AUTH_REQ_CHANNEL = int(environ.get("AUTH_REQ_CHANNEL", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002179479692"))
+LOG_API_CHANNEL = int(environ.get("LOG_API_CHANNEL", "-1002470952565"))
+LOG_VR_CHANNEL = int(environ.get("LOG_VR_CHANNEL", "-1002471108414"))
 
-# MongoDB 
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+# MongoDB
+DATABASE_URI = environ.get("DATABASE_URI", "")
+DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
 
 # Files index database url
-FILES_DATABASE = environ.get('FILES_DATABASE', "")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+FILES_DATABASE = environ.get("FILES_DATABASE", "")
+COLLECTION_NAME = environ.get("COLLECTION_NAME", "Telegram_files")
 
 # Other Channel's
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002041612661'))
-DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','-1002224752949'))
-request_channel = environ.get('REQUEST_CHANNEL', '-1002478884839') 
-REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002296473971')) 
+SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "-1002041612661"))
+DELETE_CHANNELS = int(environ.get("DELETE_CHANNELS", "1002224752949"))
+request_channel = environ.get("REQUEST_CHANNEL", "-1002478884839")
+REQUEST_CHANNEL = (
+    int(request_channel)
+    if request_channel and id_pattern.search(request_channel)
+    else None
+)
+MOVIE_UPDATE_CHANNEL = int(environ.get("MOVIE_UPDATE_CHANNEL", "-1002296473971"))
 
-# Added Link Here Not Id 
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/YaduvanshiXsupport')
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/B4UMovieZone')
+# Added Link Here Not Id
+SUPPORT_CHAT = environ.get("SUPPORT_CHAT", "https://t.me/YaduvanshiXsupport")
+MOVIE_GROUP_LINK = environ.get("MOVIE_GROUP_LINK", "https://t.me/B4UMovieZone")
 
 # Verification
-IS_VERIFY = is_enabled('IS_VERIFY', False)
+IS_VERIFY = is_enabled("IS_VERIFY", True)
+# ---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/How_to_open_movielink/15")
 TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/How_to_open_movielink/17")
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/How_to_open_movielink/22")
-VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
+VERIFY_IMG = environ.get(
+    "VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg"
+)
 SHORTENER_API = environ.get("SHORTENER_API", "597e5e39d64825dedf5b43361e9a57e5d1ab6be5")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'shortxlinks.com')
-SHORTENER_API2 = environ.get("SHORTENER_API2", "4030dd8b87e40fb381a15ce48c4a56555bed524e")
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'arlinks.in')
-SHORTENER_API3 = environ.get("SHORTENER_API3", "c4fea41840e9405644de2572c5312a19a9e41286")
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", 'youlinks.in')
-TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "14400"))
-THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "21600"))
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "shortxlinks.com")
+SHORTENER_API2 = environ.get(
+    "SHORTENER_API2", "4030dd8b87e40fb381a15ce48c4a56555bed524e"
+)
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "arlinks.in")
+SHORTENER_API3 = environ.get(
+    "SHORTENER_API3", "c4fea41840e9405644de2572c5312a19a9e41286"
+)
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "youlinks.in")
+TWO_VERIFY_GAP = int(environ.get("TWO_VERIFY_GAP", "14400"))
+THREE_VERIFY_GAP = int(environ.get("THREE_VERIFY_GAP", "21600"))
 
 # Language & Quality & Season & Year
-LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam", "bengali", "marathi", "gujarati", "punjabi", "marathi"]
-QUALITIES = ["HdRip","web-dl" ,"bluray", "hdr", "fhd" , "240p", "360p", "480p", "540p", "720p", "960p", "1080p", "1440p", "2K", "2160p", "4k", "5K", "8K"]
-YEARS = [f'{i}' for i in range(2025, 2002,-1 )]
-SEASONS = [f'season {i}'for i in range (1 , 23)]
+LANGUAGES = [
+    "hindi",
+    "english",
+    "telugu",
+    "tamil",
+    "kannada",
+    "malayalam",
+    "bengali",
+    "marathi",
+    "gujarati",
+    "punjabi",
+    "marathi",
+]
+QUALITIES = [
+    "HdRip",
+    "web-dl",
+    "bluray",
+    "hdr",
+    "fhd",
+    "240p",
+    "360p",
+    "480p",
+    "540p",
+    "720p",
+    "960p",
+    "1080p",
+    "1440p",
+    "2K",
+    "2160p",
+    "4k",
+    "5K",
+    "8K",
+]
+YEARS = [f"{i}" for i in range(2025, 2002, -1)]
+SEASONS = [f"season {i}" for i in range(1, 23)]
 
 # Pictures And Reaction
-START_IMG = (environ.get('START_IMG', 'https://te.legra.ph/file/b4a3747fc57c0a78b8ce7.jpg https://te.legra.ph/file/96bfa2b897869b53f3c1b.jpg https://telegra.ph/file/ab7c45feabcd446799ec9.jpg https://telegra.ph/file/be0e05a69125057f7403d.jpg https://telegra.ph/file/083d98858adfc45ce1761.jpg https://telegra.ph/file/c3e7e855f6fe2deb7fe21.jpg https://telegra.ph/file/966dae7b6f91a58aa037c.jpg https://telegra.ph/file/fa74ca254cc4368c167cd.jpg https://telegra.ph/file/08e2e68f2d9c959ca30fc.jpg https://telegra.ph/file/1d63c8f8102d8bf6dd90f.jpg https://envs.sh/Wza.jpg https://files.catbox.moe/kf2c2z.jpg https://files.catbox.moe/tljzgo.jpg https://files.catbox.moe/gtonu2.jpg https://files.catbox.moe/4az6sz.jpg https://files.catbox.moe/ceg0l0.jpg')).split()
-FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
-REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
-PAYPICS = (environ.get('PAYPICS', 'https://envs.sh/WzX.jpg')).split()
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://envs.sh/WzO.jpg'))
+START_IMG = (
+    environ.get(
+        "START_IMG",
+        "https://te.legra.ph/file/b4a3747fc57c0a78b8ce7.jpg https://te.legra.ph/file/96bfa2b897869b53f3c1b.jpg https://envs.sh/QL-.jpg https://telegra.ph/file/ab7c45feabcd446799ec9.jpg https://telegra.ph/file/be0e05a69125057f7403d.jpg https://telegra.ph/file/083d98858adfc45ce1761.jpg https://telegra.ph/file/c3e7e855f6fe2deb7fe21.jpg https://telegra.ph/file/966dae7b6f91a58aa037c.jpg https://telegra.ph/file/fa74ca254cc4368c167cd.jpg https://telegra.ph/file/08e2e68f2d9c959ca30fc.jpg https://telegra.ph/file/1d63c8f8102d8bf6dd90f.jpg https://envs.sh/Wza.jpg https://files.catbox.moe/kf2c2z.jpg https://files.catbox.moe/tljzgo.jpg https://files.catbox.moe/gtonu2.jpg https://files.catbox.moe/4az6sz.jpg https://files.catbox.moe/ceg0l0.jpg",
+    )
+).split()
+FORCESUB_IMG = environ.get("FORCESUB_IMG", "https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg")
+REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split()
+PAYPICS = (
+    environ.get("PAYPICS", "https://envs.sh/WzX.jpg")
+).split()
+SUBSCRIPTION = environ.get(
+    "SUBSCRIPTION", "https://envs.sh/WzO.jpg"
+)
 REACTIONS = ["👀", "😈", "🔥", "😍", "🎉", "🥰", "❤️", "🤠", "😘", "🙏", "🤗", "😻", "🌹", "👻", "🌚", "🤡", "😇", "⚡"]
 
 
-#Other Funtions
-FILE_AUTO_DEL_TIMER = int(environ.get('FILE_AUTO_DEL_TIMER', '21600'))
-AUTO_FILTER = is_enabled('AUTO_FILTER', True)
-IS_PM_SEARCH = is_enabled('IS_PM_SEARCH', False)
-IS_SEND_MOVIE_UPDATE = is_enabled('IS_SEND_MOVIE_UPDATE', False) # Don't Change It ( If You Want To Turn It On Then Turn It On By Commands) We Suggest You To Make It Turn Off If You Are Indexing Files First Time.
-MAX_BTN = int(environ.get('MAX_BTN', '8'))
-AUTO_DELETE = is_enabled('AUTO_DELETE', True)
-DELETE_TIME = int(environ.get('DELETE_TIME', 1800))
-IMDB = is_enabled('IMDB', False)
-FILE_CAPTION = environ.get('FILE_CAPTION', f'{script.FILE_CAPTION}')
-IMDB_TEMPLATE = environ.get('IMDB_TEMPLATE', f'{script.IMDB_TEMPLATE_TXT}')
-LONG_IMDB_DESCRIPTION = is_enabled('LONG_IMDB_DESCRIPTION', False)
-PROTECT_CONTENT = is_enabled('PROTECT_CONTENT', False)
-SPELL_CHECK = is_enabled('SPELL_CHECK', True)
-LINK_MODE = is_enabled('LINK_MODE', True)
+# Other Funtions
+FILE_AUTO_DEL_TIMER = int(environ.get("FILE_AUTO_DEL_TIMER", "21600"))
+AUTO_FILTER = is_enabled("AUTO_FILTER", True)
+IS_PM_SEARCH = is_enabled("IS_PM_SEARCH", False)
+IS_SEND_MOVIE_UPDATE = is_enabled(
+    "IS_SEND_MOVIE_UPDATE", False
+)  # Don't Change It ( If You Want To Turn It On Then Turn It On By Commands) We Suggest You To Make It Turn Off If You Are Indexing Files First Time.
+MAX_BTN = int(environ.get("MAX_BTN", "8"))
+AUTO_DELETE = is_enabled("AUTO_DELETE", True)
+DELETE_TIME = int(environ.get("DELETE_TIME", 1800))
+IMDB = is_enabled("IMDB", False)
+FILE_CAPTION = environ.get("FILE_CAPTION", f"{script.FILE_CAPTION}")
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")
+LONG_IMDB_DESCRIPTION = is_enabled("LONG_IMDB_DESCRIPTION", False)
+PROTECT_CONTENT = is_enabled("PROTECT_CONTENT", False)
+SPELL_CHECK = is_enabled("SPELL_CHECK", True)
+LINK_MODE = is_enabled("LINK_MODE", True)
 TMDB_API_KEY = environ.get("TMDB_API_KEY", "")
 
-# Online Streaming And Download 
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or Flase
+# Online Streaming And Download
+STREAM_MODE = bool(environ.get("STREAM_MODE", True))  # Set True or Flase
 
 MULTI_CLIENT = False
-SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
+SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
+if "DYNO" in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
@@ -114,32 +177,32 @@ URL = environ.get("FQDN", "")
 
 # Commands
 admin_cmds = [
-    "/add_premium - Add A User To Premium", 
-    "/premium_users - View All Premium Users", 
-    "/remove_premium - Remove A User's Premium Status", 
+    "/add_premium - Add A User To Premium",
+    "/premium_users - View All Premium Users",
+    "/remove_premium - Remove A User's Premium Status",
     "/add_redeem - Generate A Redeem Code",
-    "/refresh - Refresh Free Trail", 
-    "/set_muc - Set Movie Update Channel", 
-    "/pm_search_on - Enable PM Search", 
+    "/refresh - Refresh Free Trail",
+    "/set_muc - Set Movie Update Channel",
+    "/pm_search_on - Enable PM Search",
     "/pm_search_off - Disable PM Search",
-    "/set_ads - Set Advertisements", 
-    "/del_ads - Delete Advertisements", 
-    "/setlist - Set Top Trending List", 
+    "/set_ads - Set Advertisements",
+    "/del_ads - Delete Advertisements",
+    "/setlist - Set Top Trending List",
     "/clearlist - Clear Top Trending List",
-    "/verify_id - Verification Off ID", 
-    "/index - Index Files", 
-    "/send - Send Message To A User", 
+    "/verify_id - Verification Off ID",
+    "/index - Index Files",
+    "/send - Send Message To A User",
     "/leave - Leave A Group Or Channel",
-    "/ban - Ban A User", 
-    "/unban - Unban A User", 
-    "/broadcast - Broadcast Message", 
+    "/ban - Ban A User",
+    "/unban - Unban A User",
+    "/broadcast - Broadcast Message",
     "/grp_broadcast - Broadcast Messages To Groups",
-    "/delreq - Delete Join Request", 
-    "/channel - List Of Database Channels", 
-    "/del_file - Delete A Specific File", 
+    "/delreq - Delete Join Request",
+    "/channel - List Of Database Channels",
+    "/del_file - Delete A Specific File",
     "/delete - Delete A File(By Reply)",
-    "/deletefiles - Delete Multiple Files", 
-    "/deleteall - Delete All Files"
+    "/deletefiles - Delete Multiple Files",
+    "/deleteall - Delete All Files",
 ]
 
 cmds = [
@@ -157,6 +220,5 @@ cmds = [
     {"details": "Check Group Details"},
     {"settings": "Change Bot Setting"},
     {"grp_cmds": "Check Group Commands"},
-    {"admin_cmds": "Bot Admin Commands"}
+    {"admin_cmds": "Bot Admin Commands"},
 ]
-
